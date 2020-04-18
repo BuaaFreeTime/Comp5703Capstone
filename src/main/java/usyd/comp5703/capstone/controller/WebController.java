1168b80e22cf33b7a8c7d6e41ad1f4e31c22e001
RyanAsher95 <asherryan95@outlook.com>
2020-04-18 16:09:57 +0800

    Merge branch 'master' of C:\Users\Ryan\Desktop\20S1\5703\CS68\project\Comp5703Capstone with conflicts.

--- src/main/java/usyd/comp5703/capstone/controller/WebController.java
+++ src/main/java/usyd/comp5703/capstone/controller/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
-    @RequestMapping(value = {"", "/",  "/student"})
+    @RequestMapping(value = {"", "/"})
     public String loginStudent() {
         return "login-student";
     }
