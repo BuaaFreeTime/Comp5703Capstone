75374ca4e647786c0997e1ac53e307551df17c7d
freetime <ltan8436@uni.sydney.edu.au>
2020-04-29 13:16:41 +0800

    Fix BUG

--- src/main/java/usyd/comp5703/capstone/controller/ProjectController.java
+++ src/main/java/usyd/comp5703/capstone/controller/ProjectController.java
@@ -1,6 +1,5 @@
 package usyd.comp5703.capstone.controller;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
