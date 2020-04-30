a4150c41e28cfe15d8c3b4069c5cd1744651e596
freetime <ltan8436@uni.sydney.edu.au>
2020-04-30 21:38:00 +0800

    Add Interceptor

---
+++ src/main/java/usyd/comp5703/capstone/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package usyd.comp5703.capstone;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        HttpSession session = request.getSession();
+
+        // get whether the user login or not
+        Object userInfo = session.getAttribute("userInfo");
+        if (userInfo == null) {
+            System.out.println("xxxxxxxxxxxxxxx");
+            return false;
+        }
+        System.out.println("222222222222222");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+
+    }
+}
