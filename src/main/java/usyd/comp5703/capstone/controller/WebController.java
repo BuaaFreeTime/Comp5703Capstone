186d266c4da90f3b0c77c2e2aecb854634abca9d
wzh0313 <wzh930313@hotmail.com>
2020-05-04 14:49:25 +1000

    Update webcontroller

--- src/main/java/usyd/comp5703/capstone/controller/WebController.java
+++ src/main/java/usyd/comp5703/capstone/controller/WebController.java
@@ -19,13 +19,16 @@ public class WebController {
     @RequestMapping(value = {"/admin"})
     public String loginAdmin() { return "login-admin"; }
 
-    @RequestMapping(value = {"/index-student"})
-    public String index() { return "index-student"; }
+    @RequestMapping(value = {"/index-student", "/student/index-student"})
+    public String StudentIndex() { return "index-student"; }
 
-    @RequestMapping(value = {"/AllGroup"})
+    @RequestMapping(value = {"/MyProjects", "/student/MyProjects"})
+    public String MyProjects() { return "MyProjects"; }
+
+    @RequestMapping(value = {"/AllGroup", "/student/AllGroup"})
     public String AllGroup() { return "AllGroup"; }
 
-    @RequestMapping(value = {"/MyGroup"})
+    @RequestMapping(value = {"/MyGroup", "/student/MyGroup"})
     public String MyGroup() { return "MyGroup"; }
 
     @RequestMapping(value = {"/1"})
@@ -33,6 +36,51 @@ public class WebController {
         return "index-student";
     }
 
-    @RequestMapping(value = {"/MyProjects"})
-    public String MyProjects() { return "MyProjects"; }
+    @RequestMapping(value = {"/ProjectList", "/student/ProjectList"})
+    public String ProjectList() { return "ProjectList"; }
+
+    @RequestMapping(value = {"/ProjectPreference", "/student/ProjectPreference"})
+    public String ProjectPreference() { return "ProjectPreference"; }
+
+    @RequestMapping(value = {"/PresentationSlot", "/student/PresentationSlot"})
+    public String PresentationSlot() { return "PresentationSlot"; }
+
+    @RequestMapping(value = {"/Marks", "/student/Marks"})
+    public String Marks() { return "Marks"; }
+
+    @RequestMapping(value = {"/ScheduleTables", "/student/ScheduleTables"})
+    public String ScheduleTables() { return "ScheduleTables"; }
+
+    @RequestMapping(value = {"/index-admin"})
+    public String indexAdmin() { return "index-admin"; }
+
+    @RequestMapping(value = {"/ProposedProjects"})
+    public String ProposedProjects() { return "ProposedProjects"; }
+
+    @RequestMapping(value = {"/ProjectInformation"})
+    public String ProjectInformation() { return "ProjectInformation"; }
+
+    @RequestMapping(value = {"/UploadStudentInfo"})
+    public String UploadStudentInfo() { return "UploadStudentInfo"; }
+
+    @RequestMapping(value = {"/StudentGroup"})
+    public String StudentGroup() { return "StudentGroup"; }
+
+    @RequestMapping(value = {"/StudentPreferences"})
+    public String StudentPreferences() { return "StudentPreferences"; }
+
+    @RequestMapping(value = {"/Presentation"})
+    public String Presentation() { return "Presentation"; }
+
+    @RequestMapping(value = {"/ProjectsandClients"})
+    public String ProjectsandClients() { return "ProjectsandClients"; }
+
+    @RequestMapping(value = {"/TypesofProjects"})
+    public String TypesofProjects() { return "TypesofProjects"; }
+
+    @RequestMapping(value = {"/Marks-admin"})
+    public String MarksAdmin() { return "Marks-admin"; }
+
+    @RequestMapping(value = {"/profile"})
+    public String Profile() { return "profile"; }
 }
