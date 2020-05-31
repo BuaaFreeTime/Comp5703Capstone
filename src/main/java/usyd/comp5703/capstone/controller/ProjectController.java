e07ac00a49659789cdc961d325413724f0e11789
RyanAsher95 <asherryan95@outlook.com>
2020-05-31 16:06:02 +0800

    unblock update

--- src/main/java/usyd/comp5703/capstone/controller/ProjectController.java
+++ src/main/java/usyd/comp5703/capstone/controller/ProjectController.java
@@ -42,7 +42,7 @@ public class ProjectController {
                                     @RequestParam("clientid") String clientid,
                                     @RequestParam("tutor") String tutor,
                                      @RequestParam("pid") String pid) {
-//        projectService.updateProjectAdmin(unit, type, name, description,clientid,tutor, pid);
+        projectService.updateProjectAdmin(unit, type, name, description,clientid,tutor, pid);
         return "redirect:/projectinformation";
     }
 
